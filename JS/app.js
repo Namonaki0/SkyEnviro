e15f31f690f6fee623e776ec5b78fa1f89816f73
Andre <andreferreira@live.co.uk>
2021-07-23 23:56:13 +0100

    temps layout

--- JS/app.js
+++ JS/app.js
@@ -1,16 +1,32 @@
 import { apiKey } from "./key.js";
+const output = document.getElementById("output");
 const key = apiKey;
 
 async function fetchCity() {
   const api = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=lisbon&appid=${key}`
+    `https://api.openweathermap.org/data/2.5/weather?q=Lisbon&appid=${key}`
   );
-  const result = await api.json();
-  document.write(result.weather[0].description);
-  // document.write(result.weather.description);
-  // document.write(result.weather.icon);
+  const data = await api.json();
 
-  console.log(result.weather[0].icon);
+  const temp = await fetch(
+    `https://api.openweathermap.org/data/2.5/find?q=Lisbon&units=metric&appid=${key}`
+  );
+  const result = await temp.json();
+
+  output.innerHTML = `
+    <h1>${data.name}</h1> 
+    <h2>${result.list[0].main.temp} &deg;</h2>
+    <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"></img>
+    <div>${data.weather[0].description}</div> 
+    <div class="min-max-temps">
+      <div><span>min:</span> ${result.list[0].main.temp_min}&deg;</div> 
+      <div><span>max:</span> ${result.list[0].main.temp_max}&deg;</div> 
+    </div>
+    
+    
+    `;
+
+  console.log(result);
 }
 
 document.addEventListener("DOMContentLoaded", fetchCity);
